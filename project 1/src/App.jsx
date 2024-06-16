b76954e97f899635ea5981c3ea96f8ed4853ca70
MR. Subi <bantawasubin@gmail.com>
2024-06-16 16:55:08 +0545

    project 1 Shoes website

---
+++ project 1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import HeroSection from './components/Hero.jsx';
+import Navigation from './components/Navigation.jsx';
+
+const App = ()=>{
+    return <div>
+   <Navigation/> 
+   <HeroSection/>
+
+    </div>
+}
+
+export default App;
+ 
